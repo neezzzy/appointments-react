ded17512455cc92e75c6ce341dd9fa9ede72f32e
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2022-10-10 12:48:54 -0700

    add tests for AppointmentsDayView

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from "react";
-import * as ReactDOM from 'react-dom/client';
-import { Appointment } from "../src/Appointment";
+import * as ReactDOM from "react-dom/client";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 import { act } from "react-dom/test-utils";
 
 describe("Appointment", () => {
@@ -21,5 +21,41 @@ describe("Appointment", () => {
     customer = { firstName: "Jordan" };
     render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Jordan");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+  const today = new Date();
+  const twoAppointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) },
+  ];
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+  const render = (component) =>
+    act(() => ReactDOM.createRoot(container).render(component));
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+  it("renders an ol element to display appointments", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    const listElement = document.querySelector("ol");
+    expect(listElement).not.toBeNull();
+  });
+  it("renders an li for each appointment", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    const listChildren = document.querySelectorAll("ol > li");
+    expect(listChildren).toHaveLength(2);
+  });
+  it("renders the time of each appointment", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    const listChildren = document.querySelectorAll("li");
+    expect(listChildren[0].textContent).toEqual("12:00");
+    expect(listChildren[1].textContent).toEqual("13:00");
   });
 });
